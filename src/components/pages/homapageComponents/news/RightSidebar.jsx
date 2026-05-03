23b7fdc354cb547dd70b2dbdf21fa2cb444efddd
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-05-03 23:44:46 +0600

    added more responsiveness on login  section

--- src/components/pages/homapageComponents/news/RightSidebar.jsx
+++ src/components/pages/homapageComponents/news/RightSidebar.jsx
@@ -34,34 +34,34 @@ const RightSidebar = () => {
         <div className="flex flex-col gap-4">
           <button
             onClick={handleGoogleLogin}
-            className="btn border-blue-400 text-blue-400"
+            className="btn border-blue-400 text-blue-400 text-[10px] md:text-lg p-2 md:p-4"
           >
             <FaGoogle /> Login with Google
           </button>
-          <button className="btn" onClick={handleGithubLogin}>
+          <button className="btn text-[10px] md:text-lg p-2 md:p-4" onClick={handleGithubLogin}>
             <FaGithub /> Login with Github
           </button>
         </div>
       </div>
       <div className="space-y-3">
-        <h1 className="text-xl font-semibold">Find Us On</h1>
+        <h1 className="text-[16px] md:text-lg font-semibold">Find Us On</h1>
         <div className="border border-gray-400 rounded-lg">
-          <div className="p-3 border-b border-gray-400 flex items-start gap-3">
+          <div className="md:p-3 p-1  border-b border-gray-400 flex items-start gap-3">
             <FaFacebook color="blue" />
-            <p className="text-lg">Facebook</p>
+            <p className="text-xs md:text-lg">Facebook</p>
           </div>
-          <div className="p-3 border-b border-gray-400 flex items-start gap-3">
+          <div className="md:p-3 p-1 border-b border-gray-400 flex items-start gap-3">
             <FaTwitter color="orange" />
-            <p className="text-lg">Twitter</p>
+            <p className="text-xs md:text-lg">Twitter</p>
           </div>
-          <div className="p-3  flex items-start gap-3">
+          <div className="md:p-3 p-1  flex items-start gap-3">
             <FaInstagram color="red" />
-            <p className="text-lg">Instagram</p>
+            <p className="text-xs md:text-lg">Instagram</p>
           </div>
         </div>
       </div>
-      <div className="space-y-3 bg-base-300 rounded-lg p-5">
-        <h1 className="text-xl font-semibold">Q-Zone</h1>
+      <div className="space-y-3 bg-base-300 rounded-lg p-2 md:p-5">
+        <h1 className="text-[16px] md:text-lg font-semibold">Q-Zone</h1>
         <div className="">
           <Image src={swimmingImg} alt="swimming img"></Image>
         </div>
