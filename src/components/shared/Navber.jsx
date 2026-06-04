d482969143e6e61b964e984afe5a42ce9e6fd56f
Mohammad Abdullah <mdabdulla01715940008@gmail.com>
2026-06-04 23:59:39 +0600

    added navber ui updated

--- src/components/shared/Navber.jsx
+++ src/components/shared/Navber.jsx
@@ -1,37 +1,64 @@
-'use client'
-import Link from 'next/link';
-import React from 'react';
-import userAvatar from '@/assets/user.png'
-import Image from 'next/image';
-import MyLink from './MyLink';
-import { authClient } from '@/lib/auth-client';
-
+"use client";
+import Link from "next/link";
+import React from "react";
+import userAvatar from "@/assets/user.png";
+import Image from "next/image";
+import MyLink from "./MyLink";
+import { authClient } from "@/lib/auth-client";
 
 const Navber = () => {
-    const { data: session, isPending } = authClient.useSession();
-    const user = session?.user;
-    // console.log(isPending);
+  const { data: session, isPending } = authClient.useSession();
+  const user = session?.user;
+  // console.log(isPending);
 
-    return (
-        <div className='flex items-center px-2 justify-between container mx-auto py-5'>
-            <div className=""></div>
-            <ul className='flex items-center gap-1 md:gap-3'>
-                <li><MyLink href={'/'}>Home</MyLink></li>
-                <li><MyLink href={'/about-us'}>About</MyLink></li>
-                <li><MyLink href={'/career'}>Career</MyLink></li>
-            </ul>
-            {user ? 
-            <div className='flex items-center gap-2 md:gap-3'>
-                <h1 className='text-[12px] md:text-[14px] lg:text-[16px] text-right'>Welcome, {user?.name}</h1>
-                <Image src={user?.image } width={40} height={30} alt='user avatar img' className='rounded-full'></Image>
-                <button className='btn  bg-purple-600 text-white text-[11px] md:text-lg' onClick={async()=> await authClient.signOut()}>Logout</button>
-            </div> :
-             <div className='flex items-center gap-3'>
-                <Image src={userAvatar} width={40} height={40} alt='user avatar img'></Image>
-                <button className='btn  bg-purple-600 text-white text-[11px] md:text-lg'><Link href={'/login'}>Login</Link></button>
-            </div>}
+  return (
+    <div className="flex items-center px-2 justify-between container mx-auto py-5">
+      <div className=""></div>
+      <ul className="flex items-center gap-2 md:gap-3">
+        <li>
+          <MyLink href={"/"}>Home</MyLink>
+        </li>
+        <li>
+          <MyLink href={"/about-us"}>About</MyLink>
+        </li>
+        <li>
+          <MyLink href={"/career"}>Career</MyLink>
+        </li>
+      </ul>
+      {user ? (
+        <div className="flex items-center gap-2 md:gap-3">
+          <h1 className="text-[12px] md:text-[14px] lg:text-[16px] text-right">
+            Welcome, {user?.name}
+          </h1>
+          <Image
+            src={user?.image}
+            width={40}
+            height={30}
+            alt="user avatar img"
+            className="rounded-full"
+          ></Image>
+          <button
+            className="btn  bg-purple-600 text-white text-[11px] md:text-lg"
+            onClick={async () => await authClient.signOut()}
+          >
+            Logout
+          </button>
         </div>
-    );
+      ) : (
+        <div className="flex items-center gap-3">
+          <Image
+            src={userAvatar}
+            width={40}
+            height={40}
+            alt="user avatar img"
+          ></Image>
+          <button className="btn  bg-purple-re600 text-white text-[11px] md:text-lg">
+            <Link href={"/login"}>Login</Link>
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Navber;
